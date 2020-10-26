2912cdb29c5f129fabbd1c98641e13ec2447e8ce
Cao Mai <caomai@rocketmail.com>
2020-10-26 16:44:11 -0700

    [add] ESLint with AirBnb style

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  "plugins": [
+    "html"
+],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+
+};
